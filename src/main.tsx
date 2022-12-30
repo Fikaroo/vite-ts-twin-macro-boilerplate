23a1a6e32988219dbafceb0a7f7e3478368a3262
Fuad <fuadbayramv@gmail.com>
2022-12-30 15:53:48 +0400

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+// src/main.tsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import GlobalStyles from "./styles/GlobalStyles";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>
+);
